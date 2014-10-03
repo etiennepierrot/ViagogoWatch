7859d634e874061ae8b1c2637aa2aeb176c9c221
Etienne Pierrot <etienne.pierrot@leetchi.com>
2014-10-03 22:12:03 +0200

    first commit

---
+++ ConsoleWatcher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = @"http://www.viagogo.fr/";
+            string pathToWatch = @"psg/Billets-de-sport/Football/Ligue-1/Paris-Saint-Germain-Billets/E-667356";
+
+
+        }
+    }
+}
---
+++ ViagogoWatcher.Model.Test/ParserHelperTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViagoWatcher.Model;
+
+namespace ViagogoWatcher.Model.Test
+{
+    [TestClass]
+    public class ParserHelperTest
+    {
+        [TestMethod]
+        public void GetLowerPrice_Should_Return_Something()
+        {
+            //arrange
+            ParserHelper parserHelper = new ParserHelper();
+
+            //act
+            long result = parserHelper.GetLowerPrice("http://www.viagogo.fr/psg/Billets-de-sport/Football/Ligue-1/Paris-Saint-Germain-Billets/E-667356");
+
+            //assert
+            Assert.AreEqual(34, result);
+
+        }
+    }
+}
